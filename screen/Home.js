859c0e281d6793e47cc0b0fd90b9d060925a1f1d
NoelMiranda1 <mirandantonionoel@gmail.com>
2021-01-28 12:51:45 -0600

    Component Header

--- screen/Home.js
+++ screen/Home.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import Header from '../component/Header';
+import {icons, images, SIZES, FONTS, COLORS} from '../constants';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <Header />
+    </SafeAreaView>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 1,
+  },
+});
 
 export default Home;
